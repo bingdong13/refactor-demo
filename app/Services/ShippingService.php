1c7a917f285abfe18e470362647427ceed81de80
lvmaohai <bingdong13@msn.com>
2017-08-28 17:11:14 +0800

    重构第一式：Extract Method

--- app/Services/ShippingService.php
+++ app/Services/ShippingService.php
@@ -2,10 +2,20 @@
 declare(strict_types=1);
 
 /**
- * 改用 switch 写法会比 if else 可读性高些。
+ * 不过由 if else 变成 switch 并不算重构，只是让程序稍微好阅读些而已。
  *
- * PhpStorm 也提供工具可以直接将 if else 转成 switch，
- * 按热键 ⌥(option) + ↩(return)，选择 Replace if with switch。
+ * 第一式 : Extract Method
+ *
+ * 所有的重构，都是从 Extract Method 开始，当我们发现程序中有以下特征：
+ * 1. 当一段程序需要写注释特别解释时。
+ * 2. 在 if else 内有一段逻辑时。
+ * 3. 在 switch case 内有一段逻辑时。
+ *
+ * 就可以开始使用重构第一式，将一段程序重构成 method。
+ *
+ * PhpStorm 內建支持 Extract Method，先选择要抽取的程序，
+ * 按热键 ⌃(control) + T，选择 Method，
+ * 就会自动帮你將那段程序 extract 成新的 method。
  */
 
 namespace App\Services;
@@ -17,7 +27,7 @@ namespace App\Services;
  *
  * @package   ShippingService.php
  * @author    lvmaohai <lvmaohai@vpgame.cn>
- * @version   v0.1.1 2017/8/28 16:59
+ * @version   v0.1.2 2017/8/28 17:10
  */
 class ShippingService
 {
@@ -36,40 +46,81 @@ class ShippingService
         switch ($companyName) {
             case 'CaiNiao':
 
-                $weights = collect($weights);
-
-                foreach ($weights as $weight) {
-                    $amount = $amount + (100 + $weight * 10);
-                }
+                $amount = $this->caiNiaoCalculateFee($weights, $amount);
 
                 break;
             case 'JingDong':
 
-                $weights = collect($weights);
-
-                foreach ($weights as $weight) {
-                    $amount = $amount + (80 + $weight * 15);
-                }
+                $amount = $this->JingDongCalculateFee($weights, $amount);
 
                 break;
             case 'PostOffice':
 
-                $weights = collect($weights);
-
-                foreach ($weights as $weight) {
-                    $amount = $amount + (60 + $weight * 20);
-                }
+                $amount = $this->postCalculateFee($weights, $amount);
 
                 break;
             default:
 
-                $weights = collect($weights);
-
-                foreach ($weights as $weight) {
-                    $amount = $amount + (100 + $weight * 10);
-                }
+                $amount = $this->caiNiaoCalculateFee($weights, $amount);
 
                 break;
+        }
+
+        return $amount;
+    }
+
+    /**
+     * 计算菜鸟运费
+     *
+     * @param array $weights
+     * @param       $amount
+     *
+     * @return int
+     */
+    private function caiNiaoCalculateFee(array $weights, int $amount): int
+    {
+        $weights = collect($weights);
+
+        foreach ($weights as $weight) {
+            $amount = $amount + (100 + $weight * 10);
+        }
+
+        return $amount;
+    }
+
+    /**
+     * 计算京东运费
+     *
+     * @param array $weights
+     * @param int   $amount
+     *
+     * @return int
+     */
+    private function JingDongCalculateFee(array $weights, int $amount): int
+    {
+        $weights = collect($weights);
+
+        foreach ($weights as $weight) {
+            $amount = $amount + (80 + $weight * 15);
+        }
+
+        return $amount;
+    }
+
+    /**
+     * 计算邮局运费
+     *
+     * @param array $weights
+     * @param int   $amount
+     *
+     * @return int
+     */
+    private function postCalculateFee(array $weights, int $amount): int
+    {
+        $weights = collect($weights);
+
+        foreach ($weights as $weight) {
+            $amount = $amount + (60 + $weight * 20);
         }
 
         return $amount;
